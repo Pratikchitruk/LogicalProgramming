b6735d8bac6732118bd1ed922040e251f5a392f6
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-25 12:52:30 +0530

    Program.cs push for logical programming

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    namespace logical_programming
+
+    {
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welocome to logical programming");
+            reverse.ReverseTheNumber();
+            Fibonacci.DisplayFiboacci();
+            PerfectNo.FindPerfeftNo();
+            PrimeNo.FindThePrimeNo();
+            SimulatingStopWatch.stopwatch();
+
+
+        }
+}
+}
