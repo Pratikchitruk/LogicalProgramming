5248310b9fb04faddc486481d734cabbbe2bfb53
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-26 19:06:43 +0530

    push for logical programming

---
+++ SimulateStopwatchProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace logical_programming
+{
+    public class SimulatingStopWatch
+    {
+        public static void stopwatch()
+        {
+
+            Stopwatch stopwatch = new Stopwatch();
+
+
+            stopwatch.Start();
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1);
+            }
+
+
+            stopwatch.Stop();
+
+
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
---
+++ reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_programming
+{
+    public class reverse
+    {
+        public static void ReverseTheNumber()
+        {
+            int n, reverse = 0, rem;
+
+            Console.WriteLine("Enter the no.");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse*10 + rem;
+                n /= 10;
+
+            }
+            Console.WriteLine("reverse the no." + reverse);
+        }
+    }
+}
