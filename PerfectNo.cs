ab2e4477715a30cbffbe16c4f2f9d7025c0ede3c
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-26 19:03:17 +0530

    PerfectNo.cs  push for logical programming

---
+++ PerfectNo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_programming
+{
+   public class PerfectNo
+    {
+        public static void FindPerfeftNo()
+        {
+            int num, sum, mn, mx;
+            Console.WriteLine("Finding perfect no. in given serise");
+            Console.WriteLine("Enter a no");
+            mn = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter ending no");
+            mx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Perfect number are :");
+
+            for (num = mn; num <= mx; num++)
+            {
+                int i = 1;
+                sum = 0;
+                while (i < num)
+                {
+                    if (num % i == 0) 
+                    sum += i;
+                    i++;
+
+                }
+                if (sum == num)
+                    Console.WriteLine(sum);
+
+
+            }
+            
+        }
+    }
+}
